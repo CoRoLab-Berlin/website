ee1cf2126e45add0ceac76b33b33f967ab666b00
Tobias Reich <tobias@mreich.de>
2024-10-10 12:59:32 +0200

    Add Logos component

---
+++ components/Logos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-white-50 py-10 2xl:py-16 xl:py-8">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">In Cooperation with</h2>
+        <div class="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none">
+          <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://www.biorobotiklabor.de/images/BHT_Logo_kompakt_horizontal_Anthrazit_RGB_144ppi.png" alt="Transistor" width="158" height="48" />
+          <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://www.biorobotiklabor.de/images/TU-Berlin_360x114.png" alt="Reform" width="158" height="48" />
+          <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://www.biorobotiklabor.de/images/VDI-VDE-IT_200x63.png" alt="Tuple" width="158" height="48" />
+          <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://www.biorobotiklabor.de/images/Festo_200x63.png" alt="SavvyCal" width="158" height="48" />
+          <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://www.biorobotiklabor.de/images/SachEng._400x175.jpg" alt="Statamic" width="158" height="48" />
+          <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://www.biorobotiklabor.de/images/CarlStahl_310x135.png" alt="Statamic" width="158" height="48" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
