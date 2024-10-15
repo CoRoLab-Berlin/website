d67f4ad4833c218f9e3a09bee25b191a2d516b43
Tobias Reich <tobias@mreich.de>
2024-10-15 14:09:02 +0200

    Try crawlLinks option in prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
+  },
 
   app: {
     baseURL: "/corolab_website/",
