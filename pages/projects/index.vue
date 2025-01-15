87f52bae74852346ff1fd7e1e65a6b16be00aa50
Tobias Reich <tobias@mreich.de>
2025-01-15 10:44:40 +0100

    Use work card for projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,51 +1,49 @@
 <template>
-  <div class=" py-24 sm:py-32">
+  <div class="py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
-        <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Current and past projects in our lab
         </h2>
       </div>
       <div
-        class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        class="mx-auto mt-10 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none"
       >
-        <article
-          v-for="project in projects"
-          :key="project.id"
-          class="flex max-w-xl flex-col items-start justify-between"
-        >
-          <NuxtImg
-            :src="project.image"
-            class="w-full sm:aspect-[2/1] lg:aspect-[3/2] rounded-2xl mb-4 object-cover"
-          />
-          <div class="flex items-center gap-x-4 text-xs">
-            <span class="text-bhtyellow-500">{{ project.timespan }}</span>
-            <div
-              class="relative z-10 rounded-full bg-bhtgray-600 ring-1 ring-bhtgray-500 px-3 py-1.5 font-medium text-bhtyellow-500 hover:bg-gray-100"
-            >
-              {{ project.status }}
+        <div class="grid md:grid-cols-3 gap-6">
+          <div
+            v-for="(project, index) in projects"
+            :key="index"
+            class="max-w-sm bg-bhtgray-500 border border-bhtgray-400 rounded-lg shadow"
+          >
+            <NuxtImg
+              :src="project.image"
+              class="w-full sm:aspect-[2/1] lg:aspect-[3/2] rounded-t-lg mb-4 object-cover"
+            />
+            <div class="p-5">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                {{ project.title }}
+              </h5>
+              <p class="mb-3 font-normal text-gray-700">
+                {{ project.description }}
+              </p>
+              <NuxtLink
+                :to="project._path"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-bhtgray-600 rounded-lg hover:shadow-md text-bhtyellow-500"
+              >
+                Read more
+                <ArrowRightIcon class="w-4 h-4 ml-2" />
+              </NuxtLink>
             </div>
           </div>
-          <NuxtLink :to="project._path" class="group relative">
-            <h3
-              class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
-            >
-              <a href="#">
-                <span class="absolute inset-0" />
-                {{ project.title }}
-              </a>
-            </h3>
-            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-              {{ project.description }}
-            </p>
-          </NuxtLink>
-        </article>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ArrowRightIcon } from "@heroicons/vue/16/solid";
+
 const { data: projects } = await useAsyncData(() => {
   return queryContent("/projects")
     .find()
