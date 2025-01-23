7fd4e2bd387d7a85db25a86fb9c06228aa98d72b
Tobias Reich <tobias@mreich.de>
2025-01-23 10:06:22 +0100

    Replace publications accordion with selectable list

--- pages/publications.vue
+++ pages/publications.vue
@@ -8,71 +8,51 @@
       </div>
       <div class="mx-auto lg:mx-0">
         <p class="text-gray-900 mt-10 font-normal text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, iusto consectetur nobis eaque explicabo harum voluptatibus nihil? Doloremque, ipsam tempora. Deleniti unde nesciunt tenetur dolore excepturi facilis veniam asperiores maiores iure porro. Nulla, repudiandae nobis. Recusandae dolorem consectetur alias deserunt aliquid fugiat nostrum cupiditate, odit at corrupti quo. Aliquid, consequuntur! Maiores minima fugit repellendus neque perferendis eaque officia quisquam excepturi voluptatem commodi quod accusantium aut quam labore, distinctio sapiente vitae exercitationem qui nesciunt? Soluta dolor laboriosam praesentium eveniet fuga culpa nam. Laboriosam aspernatur perferendis voluptates, velit id molestiae quis! Repellat quo dolorem sit veritatis magnam placeat omnis odit voluptas consequuntur!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          iusto consectetur nobis eaque explicabo harum voluptatibus nihil?
+          Doloremque, ipsam tempora. Deleniti unde nesciunt tenetur dolore
+          excepturi facilis veniam asperiores maiores iure porro. Nulla,
+          repudiandae nobis. Recusandae dolorem consectetur alias deserunt
+          aliquid fugiat nostrum cupiditate, odit at corrupti quo. Aliquid,
+          consequuntur! Maiores minima fugit repellendus neque perferendis eaque
+          officia quisquam excepturi voluptatem commodi quod accusantium aut
+          quam labore, distinctio sapiente vitae exercitationem qui nesciunt?
+          Soluta dolor laboriosam praesentium eveniet fuga culpa nam. Laboriosam
+          aspernatur perferendis voluptates, velit id molestiae quis! Repellat
+          quo dolorem sit veritatis magnam placeat omnis odit voluptas
+          consequuntur!
         </p>
       </div>
-      <div
-        class="mx-auto mt-10 sm:mt-16 lg:mx-0 lg:max-w-none"
-      >
-        <div class="mt-6 border border-slate-200 rounded-lg px-4">
-          <div
-            v-for="(publicationCategory, index) in publicationCategories"
-            :key="index"
-            class="border-b border-slate-200"
-          >
-            <button
-              class="w-full flex justify-between items-center py-5 text-slate-800"
-              @click="toggleAccordion(index)"
-            >
-              <span>{{ publicationCategory.title }}</span>
-              <span
-                :class="[
-                  'text-slate-800 transition-transform duration-300',
-                  activeAccordion === index ? 'rotate-45' : 'rotate-0',
-                ]"
-              >
-                <PlusIcon class="w-5 h-5" />
-              </span>
-            </button>
-            <div
-              :style="{
-                maxHeight:
-                  activeAccordion === index ? contentHeight[index] : '0px',
-              }"
-              class="max-h-0 overflow-hidden transition-all duration-300 ease-in-out"
-            >
-              <div class="pb-5 text-sm text-slate-500">
-                <ContentRendererMarkdown
-                  class="prose prose-xl prose-h2:text-2xl prose-headings:no-underline prose-h2:no-underline prose-a:font-semibold prose-bht !mx-0 max-w-none"
-                  :value="publicationCategory"
-                />
-              </div>
-            </div>
-          </div>
+      <div class="w-full columns-2 gap-0 mt-10">
+        <div
+          class="text-center text-lg border-2 border-bhtgray-300 border-r-bhtyellow-600 p-4 rounded-l-2xl hover:bg-bhtgray-500"
+          :class="activeList == 0 ? 'border-bhtyellow-600' : ''"
+          @click = "activeList = 0"
+        >
+          PAPER
+        </div>
+        <div
+          class="text-center text-lg border-2 border-bhtgray-300 border-l-0 p-4 rounded-r-2xl hover:bg-bhtgray-500"
+          :class="activeList == 1 ? 'border-bhtyellow-600' : ''"
+          @click = "activeList = 1"
+        >
+          THESES
         </div>
       </div>
+      <div class="text-sm text-slate-500 mt-10">
+                <ContentRendererMarkdown
+                  class="prose prose-xl prose-h2:text-2xl prose-headings:no-underline prose-h2:no-underline prose-a:font-semibold prose-bht !mx-0 max-w-none"
+                  :value="publicationCategories[activeList]"
+                />
+              </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { PlusIcon } from "@heroicons/vue/16/solid";
-
 const { data: publicationCategories } = await useAsyncData(() => {
   return queryContent("/publications").find();
 });
 
-const activeAccordion = ref<null | number>(null);
-const contentHeight = ref<string[]>([]);
-
-const toggleAccordion = async (index: number) => {
-  if (activeAccordion.value === index) {
-    activeAccordion.value = null;
-  } else {
-    activeAccordion.value = index;
-    await nextTick();
-    const content = document.getElementsByClassName("overflow-hidden")[index];
-    contentHeight.value[index] = content.scrollHeight + "px";
-  }
-};
+const activeList = ref<number>(0);
 </script>
