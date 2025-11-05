3b2d1c4c4bd9d2868d6f50d4ebc3381c9dcde69f
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2025-11-05 17:33:51 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,50 +1,17 @@
 <!-- eslint-disable vue/html-self-closing -->
+
 <template>
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:mx-0">
+<!--  <div class="mx-auto max-w-2xl lg:mx-0"> -->
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Contact Information
         </h2>
-      </div>
+<!--  </div> -->
       <!-- Contact Section -->
       <div
         class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:gap-x-16"
       >
-        <!-- Postal Address -->
-<!--        <div>
-          <h2 class="text-xl font-semibold mb-4">Postal Address</h2>
-          <p class="text-gray-700">
-            <NuxtLink
-              to="https://www.bht-berlin.de/"
-              class="text-blue-500 hover:underline"
-              target="_blank"
-              ><strong>Berliner Hochschule für Technik (BHT)</strong>
-            </NuxtLink>
-            <br />
-            Poststelle, FB7, HROB, Prof.Boblan
-            <br />
-            <NuxtLink
-              to="https://www.bht-berlin.de/lageplan"
-              class="text-blue-500 hover:underline"
-              target="_blank"
-              >Luxemburger Str. 10
-            </NuxtLink>
-            <br />
-            13353 Berlin
-            <br /><br /><br /><br /><br />
-            <br />
-          </p>
- 
-          <div class="mt-6">
-            <NuxtImg
-              src="images/Beuth_Lageplan_Campus.png"
-              alt="Teaching Map"
-              class="w-full rounded-2xl shadow aspect-[5/3]"
-            />
-          </div>
-        </div>
--->
       
         <!-- Teaching & Research Address -->
         <div>
