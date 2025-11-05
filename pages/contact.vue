e83187c73245e05987db0442f56b23beec49a99d
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2025-11-05 17:48:05 +0100

    Update contact.vue
    table delete

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,7 +10,7 @@
 <!--  </div> -->
       <!-- Contact Section -->
       <div
-        class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:gap-x-16"
+<!--        class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:gap-x-16" -->
       >
       
         <!-- Teaching & Research Address -->
