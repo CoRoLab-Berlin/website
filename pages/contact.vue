f276ab0421329890562e54cd69b56134c4f0238f
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2025-11-05 17:55:51 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,16 +3,11 @@
 <template>
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-<!--  <div class="mx-auto max-w-2xl lg:mx-0"> -->
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Contact Information
-        </h2>
-<!--  </div> -->
+      </h2>
       <!-- Contact Section -->
-      <div
-<!--        class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:gap-x-16" -->
-      >
-      
+    
         <!-- Teaching & Research Address -->
         <div>
           <h2 class="text-xl font-semibold mb-4">Teaching & Research Address</h2>
@@ -88,7 +83,7 @@
             />
           </div>
         </div>
-      </div>
+      
     </div>
   </div>
 </template>
