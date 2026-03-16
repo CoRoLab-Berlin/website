2f2c4a1b8ff195543387304b4d5826f801376604
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-16 22:27:44 +0100

    Remove left border from publication category divs

--- pages/publications.vue
+++ pages/publications.vue
@@ -11,14 +11,14 @@
           CONFERENCE PAPER
         </div>
         <div
-          class="text-center text-lg border-2 border-bhtgray-300 border-l-0 p-4 hover:bg-bhtgray-500 cursor-pointer"
+          class="text-center text-lg border-2 border-bhtgray-300 p-4 hover:bg-bhtgray-500 cursor-pointer"
           :class="activeList == 1 ? 'border-bhtyellow-600' : ''"
           @click="activeList = 1"
         >
           JOURNALS
         </div>
         <div
-          class="text-center text-lg border-2 border-bhtgray-300 border-l-0 p-4 hover:bg-bhtgray-500 cursor-pointer"
+          class="text-center text-lg border-2 border-bhtgray-300 p-4 hover:bg-bhtgray-500 cursor-pointer"
           :class="activeList == 2 ? 'border-bhtyellow-600' : ''"
           @click="activeList = 2"
         >
