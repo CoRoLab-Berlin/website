459f297dba50615f8e0201c400e822b3930df156
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-16 22:09:34 +0100

    Update publications.vue

--- pages/publications.vue
+++ pages/publications.vue
@@ -15,6 +15,13 @@
           :class="activeList == 1 ? 'border-bhtyellow-600' : ''"
           @click="activeList = 1"
         >
+          REPORTS
+        </div>
+        <div
+          class="text-center text-lg border-2 border-bhtgray-300 border-l-0 p-4 rounded-r-2xl hover:bg-bhtgray-500 cursor-pointer"
+          :class="activeList == 1 ? 'border-bhtyellow-600' : ''"
+          @click="activeList = 2"
+        >
           THESES
         </div>
       </div>
