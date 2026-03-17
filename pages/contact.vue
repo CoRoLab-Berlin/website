ee5214f3f2849e7574365b6209ebd66a828a86c6
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-17 18:27:49 +0100

    Update layout structure in contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,8 @@
 
 <template>
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
-    <div class="px-6 mx-auto max-w-7xl lg:px-8">
+<!--    <div class="px-6 mx-auto max-w-7xl lg:px-8">  -->
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Contact Information
       </h2>
