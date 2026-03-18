b697f6cd0e2b12d032d2243054b051d7dc3ceee3
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 15:48:10 +0100

    Update ContentSection.vue

--- components/ContentSection.vue
+++ components/ContentSection.vue
@@ -1,7 +1,6 @@
 <template>
   <div class=" bg-bhtgray-700">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      
       <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         
         <div class="lg:pr-4 h-full flex justify-center items-center">
@@ -18,7 +17,6 @@
         </div>
         
       </div>
-      
     </div>
   </div>
 </template>
