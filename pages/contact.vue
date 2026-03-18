284ddc436b6e50a8b3e1c1651c814d3569a8eb64
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 15:29:14 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
     <div class="px-6 mx-auto max-w-7xl lg:px-8">
-
-      <div class="mb-6 lg:mb-16 max-w-4xl mx-auto">
+<!-- mb-6 lg:mb-16 -->
+      <div class="max-w-4xl mx-auto">
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Test
         </h2>
