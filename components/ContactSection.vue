d4e0c30d615cadb8cba73364f886f900bfc7de5a
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 15:54:32 +0100

    Update ContactSection.vue

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -4,6 +4,7 @@
       <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
 -->
 
+<template>
   <div>
     <h2 class="text-xl font-semibold mb-4">{{ info.title }}</h2>
     
