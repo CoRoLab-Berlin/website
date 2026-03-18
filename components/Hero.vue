7500853ecd6330fc50991b0b2a5a7db1cae9e8ee
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 13:40:09 +0100

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,23 +2,18 @@
   <section class="pt-0 h-full bg-bhtgray-700">
     <div class="py-10 overflow-hidden px-5 lg:m-0 2xl:py-16 xl:py-8">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div
-          class="grid grid-cols-1 gap-14 items-center lg:grid-cols-12 lg:gap32"
-        >
-          <div
-            class="w-full xl:col-span-5 lg:col-span-6 2xl:-mx-5 xl:-mx-0 flex flex-col items-center lg:items-start"
-          >
-            <!-- <h1
-              class="py-8 text-center text-gray-900 font-bold font-manrope text-5xl lg:text-left leading-[70px] max-w-sm"
-            >
+        <div class="grid grid-cols-1 gap-14 items-center lg:grid-cols-12 lg:gap32">
+          
+          <div class="w-full xl:col-span-5 lg:col-span-6 2xl:-mx-5 xl:-mx-0 flex flex-col items-center lg:items-start" >
+            <!-- <h1 class="py-8 text-center text-gray-900 font-bold font-manrope text-5xl lg:text-left leading-[70px] max-w-sm" >
               {{ props.title }}
               <br />
             </h1> -->
             <NuxtImg
-            class=" h-48 w-auto"
-            src="images/corolab_logo.svg"
-            alt=""
-          />
+              class=" h-48 w-auto"
+              src="images/corolab_logo.svg"
+              alt=""
+            />
             <p class="text-gray-500 text-2xl text-center lg:text-left">
               {{ props.subtitle }}
             </p>
@@ -26,9 +21,10 @@
               to="/projects"
               class="bg-bhtgray-700 text-white rounded-xl cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 py-3 px-6 text-sm hover:bg-bhtyellow-500 hover:text-bhtgray-700 border-bhtyellow-500 border my-10"
             >
-              View our projects
+            View our projects
             </NuxtLink>
           </div>
+          
           <div class="w-full xl:col-span-7 lg:col-span-6 block">
             <div class="w-full sm:w-auto">
               <!-- <NuxtImg
