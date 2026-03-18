cba2c6f43df02141f4e945984d7f335ed00f795e
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 13:23:04 +0100

    Update Logos.vue

--- components/Logos.vue
+++ components/Logos.vue
@@ -2,7 +2,7 @@
   <div class="py-10 2xl:py-16 xl:py-8">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">
-        In Cooperation with
+        In cooperation with:
       </h2>
       <div
         class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none"
