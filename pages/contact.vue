24720a0ea8b04efd2fd3f7598190ac2c7bd1a010
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 15:35:25 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
-    <div class="px-6 mx-auto max-w-7xl lg:px-8">
-<!--  -->
+    <div class="px-6 mx-auto max-w-screen-xl lg:px-8">
+<!-- max-w-screen-xl max-w-7xl -->
 
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Contact Information
