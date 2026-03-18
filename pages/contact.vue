cf930fb21f453178a2ad1e527859265f9bbaaab6
IvoBoblan <156455917+IvoBoblan@users.noreply.github.com>
2026-03-18 14:35:56 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,13 +2,15 @@
   <div class="pb-24 pt-12 sm:py-32 sm:pt-16">
     <div class="px-6 mx-auto max-w-7xl lg:px-8">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact Information
+          Contact Information 1234567890 1234567890 1234567890 1234567890 12345 12345 12345
       </h2>
       
       <div>
         <div>
           
-          <h2 class="text-xl font-semibold mb-4">Teaching & Research Address</h2>
+          <h2 class="text-xl font-semibold mb-4">
+            Teaching & Research Address
+          </h2>
           <p class="text-gray-700">
             <NuxtLink
               to="https://www.bht-berlin.de/"
